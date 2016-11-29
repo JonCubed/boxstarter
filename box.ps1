64b0c86b60396d48899dfea36353aefdaa759143
Jonathan <jonathankuleff@gmail.com>
2016-11-30 08:17:15 +1100

    updated .net core vs extension

--- box.ps1
+++ box.ps1
@@ -284,7 +284,7 @@ function Install-VisualStudio
         Install-ChocolateyVsixPackage 'BuildVision' https://visualstudiogallery.msdn.microsoft.com/23d3c821-ca2d-4e1a-a005-4f70f12f77ba/file/95980/13/BuildVision.vsix
         Install-ChocolateyVsixPackage 'File Nesting' https://visualstudiogallery.msdn.microsoft.com/3ebde8fb-26d8-4374-a0eb-1e4e2665070c/file/123284/32/File%20Nesting%20v2.5.62.vsix
 
-        Install-WebPackage '.NET Core Visual Studio Extension' 'exe' '/quiet' $DownloadFolder https://go.microsoft.com/fwlink/?LinkID=827546 'DotNetCore.1.0.1-VS2015Tools.Preview2.0.2.exe' # for visual studio
+        Install-WebPackage '.NET Core Visual Studio Extension' 'exe' '/quiet' $DownloadFolder https://go.microsoft.com/fwlink/?LinkID=827546 'DotNetCore.1.0.1-VS2015Tools.Preview2.0.3.exe' # for visual studio
 
         Set-Checkpoint -CheckpointName $VSCheckpoint -CheckpointValue 1
     }
